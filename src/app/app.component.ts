b527bea583721d1b62f6a8d1cfc7418d41bb7517
williamlayadi <layadi.william123@gmail.com>
2021-02-23 17:20:21 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  hasil = 100;
+  a =0;
+  b=0;
+  item = {
+    nama:"Jeruk" , harga:5000,satuan:"kg",
+  }
+
+  itemArray = [
+    {nama:"Jeruk" , harga:5000,satuan:"kg"},
+    {nama:"Apel" , harga:15000,satuan:"kg"}
+  ]
+
+  tampil : boolean = false;
+
+  tulisanOPEN:String;
+
+  OPEN(){
+    this.tulisanOPEN = "Toko dibuka";
+    alert("HALO")
+  }
+
+  values =""
+  onKey(event:any){
+    this.values += event.target.value +"|";
+  }
+
+
 }
